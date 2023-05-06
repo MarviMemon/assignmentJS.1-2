b052c03114f406a85661f12e28b684270d1535fb
MarviMemon <marvimemon283@gmail.com>
2023-05-06 15:26:33 +0500

    this my assignment

---
+++ app.js
@@ -0,0 +1,74 @@
+
+//=======assignment one========//
+
+//======Q1=======
+//alert("Hello world");
+
+//========Q2======
+//alert("JavaScript Alert \n\n Error! Please enter a valid password");
+
+//=======Q3=======
+//alert("JavaScript Alert \n\n Welcome to JS land... \n Happy Coding!");
+
+//=====Q4=======
+//alert("JavaScript Alert \n\n Welcome to js land...");
+
+//=======Q5======
+//alert("JavaScript Alert \n\n Hello...! i can run JS through my web broeser's console.")
+
+
+
+ 
+//=======assignment two========//
+
+//Q-01
+//var userName;
+
+//=====Q2=====
+//let myName = "Marvi Memon";
+
+
+//====Q3======
+//let message = "Hello World !";
+//alert(message)
+
+
+//=====Q4=====
+//let name = "Jhone Deo";
+//let age = " 16 years old";
+//let cerificate = "Certified Mobile Application Development."
+//alert(name)
+//alert(age)
+//alert(cerificate)
+
+//=====Q5=====
+//function printPizzReverse(){
+    //let pizza = "PIZZA";
+    //for (let i = pizza.length; i > 0; i--){
+       // console.log(pizza);
+       // pizza = pizza.slice(0, i - 1) + pizza.slice(i);
+    //}
+//}
+// console.log("pizza in reverse function");
+//printPizzReverse();
+ 
+
+//=====Q6======
+//let email = "marvimemon283@gamil.com"
+//alert("My email addres is " + email);
+
+//=====Q7=====
+//let book = "A smarter way to learn JavaScript"
+//alert("I am trying to learn from the book" + book);
+
+//=====Q8======
+//document.write("Yah ! I can write HTML content through JavaScript.");
+
+//======Q9======
+//let symbol = "˚ ༘♡ ·˚꒰ᥕᥱᥣᥴ᥆꧑ᥱ t᥆ ꧑ᥡ bᥣ᥆g꒱ ₊˚ˑ༄"
+//alert(symbol)
+
+
+
+
+ 
